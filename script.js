a7e9b17b177163b8cc0fad8de443404fc218d99f
c20245832segaya-ctrl <c20245832.segaya@csav.edu.ph>
2026-09-04 16:33:20 +0800

    querySelector

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ function greet(greetings,names=[]){
 }   
 console.log(greet("Mayong aga", ["sandra", "bai", "soy", "tol", "namnueng"]));
 
+const heading = document.querySelector("h1");
+console.log(heading);
+
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading);
+
+const projectHeading = document.querySelector("#services h2");
+console.log(projectHeading);
+
