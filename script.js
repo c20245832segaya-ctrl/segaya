fdb714dc909c6aa66f2a925f423f85d0c2f6b2c6
c20245832segaya-ctrl <c20245832.segaya@csav.edu.ph>
2026-09-04 14:38:01 +0800

    variables (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log("Hello world.");
 console.log("From script.js file.");
+
+const myName="Sandra";
+let age=21;
+let number="09668930056";
+let address="Manapla"
+let addnumber=67;
+
+
+console.log(`Name: ${myName} `);
+console.log(`Age: ${age} `);
+console.log(`Number: ${number} `);
+console.log(`Address: ${address} `);
+console.log(addnumber);
+
+
+
