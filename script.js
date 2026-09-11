583344953a9b1dc7fd9b0702fa91195390434ec3
c20245832segaya-ctrl <c20245832.segaya@csav.edu.ph>
2026-09-11 15:33:09 +0800

    Set project bg to orange

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-console.log("Hello world.");
-console.log("From script.js file.");
+// console.log("Hello world.");
+// console.log("From script.js file.");
 
-const myName="Sandra";
-let age=21;
-let number="09668930056";
-let address="Manapla"
-let addnumber=67;
+// const myName="Sandra";
+// let age=21;
+// let number="09668930056";
+// let address="Manapla"
+// let addnumber=67;
 
 
-console.log(`Name: ${myName} `);
-console.log(`Age: ${age} `);
-console.log(`Number: ${number} `);
-console.log(`Address: ${address} `);
-console.log(addnumber);
+// console.log(`Name: ${myName} `);
+// console.log(`Age: ${age} `);
+// console.log(`Number: ${number} `);
+// console.log(`Address: ${address} `);
+// console.log(addnumber);
 
-function greet(greetings,names=[]){
-    if (Array.isArray(names));{
-    names=names.join(" , ")
-}
-    return  `${greetings} sa enyu, ${names}`;
+// function greet(greetings,names=[]){
+//     if (Array.isArray(names));{
+//     names=names.join(" , ")
+// }
+//     return  `${greetings} sa enyu, ${names}`;
 
-}   
-console.log(greet("Mayong aga", ["sandra", "bai", "soy", "tol", "namnueng"]));
+// }   
+// console.log(greet("Mayong aga", ["sandra", "bai", "soy", "tol", "namnueng"]));
 
-const heading = document.querySelector("h1");
-heading.textContent = "Jose Margil P. Segaya"
-heading.style.color = "purple"
-heading.style.backgroundColor = "yellow"
-heading.style.fontSize = "60px"
-console.log(heading.textContent);
+// const heading = document.querySelector("h1");
+// heading.textContent = "Jose Margil P. Segaya"
+// heading.style.color = "purple"
+// heading.style.backgroundColor = "yellow"
+// heading.style.fontSize = "60px"
+// console.log(heading.textContent);
 
-heading.addEventListener("click", function() {
-    heading.style.color = "red";
-    console.log("Heading was clicked!");
+// heading.addEventListener("click", function() {
+//     heading.style.color = "red";
+//     console.log("Heading was clicked!");
 
-});
+// });
 
-const contactHeading = document.querySelector("#contact h2");
-console.log(contactHeading);
+// const contactHeading = document.querySelector("#contact h2");
+// console.log(contactHeading);
 
-const projectHeading = document.querySelector("#services h2");
-console.log(projectHeading);
+// const projectHeading = document.querySelector("#services h2");
+// console.log(projectHeading);
 
-const toggleButton = document.querySelector ("#switch");
-const body = document.querySelector("body");
-let isOff = false;
+// const toggleButton = document.querySelector ("#switch");
+// const body = document.querySelector("body");
+// let isOff = false;
 
-toggleButton.addEventListener("click", function (){
-    isOff = !isOff;
+// toggleButton.addEventListener("click", function (){
+//     isOff = !isOff;
 
-    if (isOff){
-        body.style.backgroundColor = "black";
-        body.style.color = "white";
-    }else {
-        body.style.backgroundColor ="white";
-         body.style.color = "black";
+//     if (isOff){
+//         body.style.backgroundColor = "black";
+//         body.style.color = "white";
+//     }else {
+//         body.style.backgroundColor ="white";
+//          body.style.color = "black";
 
-      }
-});
+//       }
+// });
 
 
